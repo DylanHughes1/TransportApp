3f0357e6e767e1621e694c6427783d4f27257a78
Dylan Hughes <dylan_21174@hotmail.com>
2023-03-16 19:22:32 -0300

    Se arreglo la vista de las cards

--- resources/views/truck_driver/dashboard.blade.php
+++ resources/views/truck_driver/dashboard.blade.php
@@ -13,13 +13,14 @@
                     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 bg-white border-b border-gray-200">
                             
-                            <a href="solicitudes" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            <a href="solicitudes" style="text-decoration:none" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Ver Solicitudes</h5>
                                 <p class="font-normal text-gray-700 dark:text-gray-400">Solicitudes Pendientes</p>
                             </a>
+
                         </div>
                         <div class="p-6 bg-white border-b border-gray-200">    
-                            <a href="viajes" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            <a href="viajes" style="text-decoration:none" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Ver Viajes</h5>
                                 <p class="font-normal text-gray-700 dark:text-gray-400">Viajes realizados</p>
                             </a>                                 
--- resources/views/viajes/index.blade.php
+++ resources/views/viajes/index.blade.php
@@ -16,7 +16,7 @@
                                 
                                 @foreach ($viajes as $viaje)
                                     <div>
-                                        <a href="viajes/{{$viaje->id}} " class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                                        <a href="viajes/{{$viaje->id}}" style="text-decoration:none" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Viaje {{$viaje->fecha_salida}}</h5>
                                         </a>
                                     </div>
