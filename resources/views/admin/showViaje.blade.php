7ded920b0d7cf3b16ac0eddb34f63b6961a598d7
Dylan Hughes <dylan_21174@hotmail.com>
2023-04-03 20:19:56 -0300

    Creacion ABM viajes para admin

--- resources/views/admin/showViaje.blade.php
+++ resources/views/admin/showViaje.blade.php
@@ -17,28 +17,80 @@
                                 <table class="table">
                                     <thead>
                                     <tr>
-                                        <th class="px-6 py-2 ">Fecha Salida</th>
-                                        <th class="px-6 py-2 ">Origen</th>
-                                        <th class="px-6 py-2 ">Km Viaje</th>
-                                        <th class="px-6 py-2 ">Destino</th>
-                                        <th class="px-6 py-2 ">KM Salida</th>
-                                        <th class="px-6 py-2 ">C/Porte</th>
-                                        <th class="px-6 py-2 ">Producto</th>
-                                        <th class="px-6 py-2 ">Carga (Kg)</th>
-                                        <th class="px-6 py-2 ">Descarga (Kg)</th>
-                                        <th class="px-6 py-2 ">Control Descarga</th>
-                                        <th class="px-6 py-2 ">KM 1.2</th>
-                                        <th class="px-6 py-2 ">KM Vacíos</th>
-                                        <th class="px-6 py-2 ">Gasto en Peaje</th>
+                                        <th class="px-6 py-2 w-32">Fecha Salida</th>
+                                        <th class="px-6 py-2 w-32">Origen</th>
+                                        <th class="px-6 py-2 w-32">Fecha Llegada</th>
+                                        <th class="px-6 py-2 w-32">Km Viaje</th>
+                                        <th class="px-6 py-2 w-32">Destino</th>
+                                        <th class="px-6 py-2 w-32">KM Salida</th>
+                                        <th class="px-6 py-2 w-32">C/Porte</th>
+                                        <th class="px-6 py-2 w-32">Producto</th>
+                                        <th class="px-6 py-2 w-32">Carga (Kg)</th>
+                                        <th class="px-6 py-2 w-32">Descarga (Kg)</th>
+                                        <th class="px-6 py-2 w-32">Km Llegada</th>
+                                        <th class="px-6 py-2 w-32">Control Descarga</th>
+                                        <th class="px-6 py-2 w-32">KM 1.2</th>
+                                        <th class="px-6 py-2 w-32">KM Vacíos</th>
+                                        <th class="px-6 py-2 w-32">Gasto en Peaje</th>
+                                        <th class="px-6 py-2 w-32">Arreglo Pinchadura</th>
+                                        <th class="px-6 py-2 w-32">Retiro Plata Adelantado</th>
 
                                     </tr>
                                     </thead>
                                     <tbody class="border divide-y divide-gray-200">
                                     @foreach ($viajes as $viaje)
                                         <tr>
-                                        <td class="px-6 py-2 ">{{ $viaje->fecha_salida }}</td>
-                                        <td class="px-6 py-2 ">{{ $viaje->origen }}</td>
-                                        <td class="px-6 py-2 ">{{ $viaje->fecha_llegada }}</td>
+                                        <td class="px-6 py-2 w-32">{{ $viaje->fecha_salida }}</td>
+                                        <td class="px-6 py-2 w-32">{{ $viaje->origen }}</td>
+                                        <td class="px-6 py-2 w-32">{{ $viaje->fecha_llegada }}</td>
+                                        <td class="px-6 py-2 w-32">{{ $viaje->km_viaje }}</td>
+                                        <td class="px-6 py-2 w-32">{{ $viaje->destino }}</td>
+                                        <td class="px-6 py-2 w-32">{{ $viaje->km_salida }}</td>
+                                        <td class="px-6 py-2 w-32">{{ $viaje->c_porte }}</td>
+                                        <td class="px-6 py-2 w-32">{{ $viaje->producto }}</td>
+                                        <td class="px-6 py-2 w-32">{{ $viaje->carga_kg }}</td>
+                                        <td class="px-6 py-2 w-32">{{ $viaje->descarga_kg }}</td>
+                                        <td class="px-6 py-2 w-32">{{ $viaje->km_llegada }}</td>
+                                        <td class="px-6 py-2 w-32">{{ $viaje->control_desc }}</td>
+                                        <td class="px-6 py-2 w-32">{{ $viaje->km_1_2 }}</td>
+                                        {{-- <td class="px-6 py-2 w-32">{{ $viaje->km_vacios == 1 ? 'Si' : 'No'  }}</td> --}}
+                                        <td class="px-4 py-2 {{ $viaje->km_vacios == 0 ? 'modal-trigger text-blue-600 underline cursor-pointer' : '' }}">{{ $viaje->peaje == 1 ? 'Si' : 'No' }}</td>
+
+                                        {{-- <td class="px-4 py-2 {{ $viaje->peaje == 0 ? 'modal-trigger text-blue-600 underline cursor-pointer' : '' }}">{{ $viaje->peaje == 1 ? 'Si' : 'No' }}</td> --}}
+                                        <td class="px-4 py-2 w-32">{{ $viaje->peaje }}</td>
+                                        <td class="px-6 py-2 w-32">{{ $viaje->arreglo_pinchadura == 1 ? 'Si' : 'No' }}</td>
+                                        <td class="px-6 py-2 w-32">{{ $viaje->retiro_plata_adelanto == 1 ? 'Si' : 'No' }}</td>
+
+                                        <script>
+                                            // Busca todos los elementos con la clase "modal-trigger"
+                                            const modalTriggerElements = document.querySelectorAll('.modal-trigger');
+                                        
+                                            // Agrega un evento clic a cada elemento
+                                            modalTriggerElements.forEach((element) => {
+                                                element.addEventListener('click', (event) => {
+                                                    event.preventDefault(); // previene la acción predeterminada del enlace
+                                                    // Muestra el modal correspondiente
+                                                    $('#myModal').modal('show');
+                                                });
+                                            });
+                                        </script>
+
+                                        <div class="modal fade text-dark" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="exampleModalLabel">Observación </h5>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            
+                                                               Hola
+                                                            
+                                                        </div>           
+                                                    </div>
+                                                </div>
+                                            </div>
+
                                         </tr>
                                     @endforeach
                                     </tbody>
