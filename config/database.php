c14f90bb9e61485c05819393d515d162b1f5cca3
Dylan Hughes <dylan_21174@hotmail.com>
2023-08-03 19:28:04 -0300

    Vercel files

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,17 +65,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'key' => env('APP_KEY'),
+            'url' => env('SUPABASE_DATABASE_URL'),
+            'host' => env('SUPABASE_HOST'),
+            'port' => env('SUPABASE_PORT'),
+            'database' => env('SUPABASE_DATABASE'),
+            'username' => env('SUPABASE_USERNAME'),
+            'password' => env('SUPABASE_PASSWORD'),
+            'schema' => env('SUPABASE_SCHEMA'),
+            'sslmode' => env('SUPABASE_SSLMODE'),
         ],
 
         'sqlsrv' => [
