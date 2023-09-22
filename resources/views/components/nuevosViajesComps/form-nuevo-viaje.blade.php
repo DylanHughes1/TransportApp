a06f63d41fc5630868e5c8530eae49233b66ac51
Dylan Hughes <dylan_21174@hotmail.com>
2023-09-22 10:54:44 -0300

    Mejoras visuales

--- resources/views/components/nuevosViajesComps/form-nuevo-viaje.blade.php
+++ resources/views/components/nuevosViajesComps/form-nuevo-viaje.blade.php
@@ -29,8 +29,5 @@
     
     <div class="col-12 mt-3">
         <button type="submit" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Guardar Viaje</button>
-        <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
-            <a href="/admin/dashboard" style="text-decoration:none">Cancelar</a>
-        </button>
     </div>
 </form>
--- resources/views/components/viajesAsignadosComps/table-viajes-asignados.blade.php
+++ resources/views/components/viajesAsignadosComps/table-viajes-asignados.blade.php
@@ -29,7 +29,6 @@
                     <th cope="row" class="border-b bg-opacity-50 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
 
                         @if($viaje->progresoSolicitud === 0)
-                            {{-- {{ \App\Models\TruckDriver::find($viaje->truckdriver_id)->name ?? '' }} --}}                                
 
                             <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-gray-700 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-blue-800" type="button">
                                 <span class="mr-2">Elegir Chofer</span>
