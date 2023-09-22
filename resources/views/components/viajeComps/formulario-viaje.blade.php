0626dc1d1d7b91e63467c83586d90b2f55ef21d0
Dylan Hughes <dylan_21174@hotmail.com>
2023-09-22 18:06:16 -0300

    Fix: orden inputs de viajes

--- resources/views/components/viajeComps/formulario-viaje.blade.php
+++ resources/views/components/viajeComps/formulario-viaje.blade.php
@@ -9,18 +9,6 @@
             value="{{ $viaje->fecha_salida }}" placeholder="Año-Mes-Día" required>
         </div>
         <div>
-            <label for="destino" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                <a href="#" data-modal-toggle="myModal2" id="origen" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Destino</a>
-            </label>
-            <input type="text" name="destino" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-            value="{{ $viaje->destino }}"required="false">
-        </div>
-        <div>
-            <label for="cargaKg" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Carga Kg</label>
-            <input type="number" name="carga_kg" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            value="{{ $viaje->carga_kg }}">
-        </div>
-        <div>
             <label for="Origen" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                 <a href="#" data-modal-toggle="myModal" id="origen" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Origen</a>
             </label>
@@ -28,29 +16,16 @@
             value="{{ $viaje->origen }}" required="false">
         </div>
         <div>
-            <label for="km_salida" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Km Salida</label>
-            <input type="number" name="km_salida" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            value="{{ $viaje->km_salida }}">
-        </div>
-        <div>
-            <label for="descargaKg" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descarga Kg</label>
-            <input type="number" name="descarga_kg" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            value="{{ $viaje->descarga_kg }}">
-        </div>
-        <div>
             <label for="Fecha llegada" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fecha Llegada</label>
             <input type="date" name="fecha_llegada" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
             value="{{ $viaje->fecha_llegada }}" required="false">
         </div>
         <div>
-            <label for="porte" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Porte</label>
-            <input type="number" name="c_porte" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            value="{{ $viaje->c_porte }}">
-        </div>
-        <div>
-            <label for="kmLlegada" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Km Llegada</label>
-            <input type="number" name="km_llegada" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            value="{{ $viaje->km_llegada }}">
+            <label for="destino" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                <a href="#" data-modal-toggle="myModal2" id="origen" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Destino</a>
+            </label>
+            <input type="text" name="destino" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            value="{{ $viaje->destino }}"required="false">
         </div>
         <div>
             <label for="Km" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Km</label>
@@ -58,20 +33,46 @@
             value="{{ $viaje->km_viaje }}" readonly>
         </div>
         <div>
+            <label for="control_desc" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Control Descarga</label>
+            <input type="number" name="control_desc" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            value="{{ $viaje->control_desc }}">
+        </div>
+        <div>
+            <label for="km_salida" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Km Salida</label>
+            <input type="number" name="km_salida" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            value="{{ $viaje->km_salida }}">
+        </div>
+        <div>
+            <label for="porte" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Porte</label>
+            <input type="number" name="c_porte" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            value="{{ $viaje->c_porte }}">
+        </div>
+        <div>
             <label for="producto" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Producto</label>
             <input type="text" name="producto" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             value="{{ $viaje->producto }}">
+        </div>
+        <div>
+            <label for="cargaKg" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Carga Kg</label>
+            <input type="number" name="carga_kg" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            value="{{ $viaje->carga_kg }}">
+        </div>    
+        <div>
+            <label for="descargaKg" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descarga Kg</label>
+            <input type="number" name="descarga_kg" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            value="{{ $viaje->descarga_kg }}">
+        </div>
+        <div>
+            <label for="kmLlegada" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Km Llegada</label>
+            <input type="number" name="km_llegada" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            value="{{ $viaje->km_llegada }}">
         </div>
         <div>
             <label for="km12" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Km 1,2</label>
             <input type="number" name="km_1_2" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             value="{{ $viaje->km_1_2 }}">
         </div>
-        <div>
-            <label for="control_desc" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Control Descarga</label>
-            <input type="number" name="control_desc" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            value="{{ $viaje->control_desc }}">
-        </div>
+
     </div>
         <div class="col-span-4 md:col-span-1 flex justify-end"> 
             <a href="b/{{$viaje->id}}" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
