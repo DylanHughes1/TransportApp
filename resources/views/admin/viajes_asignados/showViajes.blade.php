7ce91ea325ef6e3c9cd3883c83274ee36b28074b
Dylan Hughes <dylan_21174@hotmail.com>
2023-10-29 17:49:53 -0300

    Fix dropdown

--- resources/views/admin/viajes_asignados/showViajes.blade.php
+++ resources/views/admin/viajes_asignados/showViajes.blade.php
@@ -44,25 +44,27 @@
 
                                         <hr class="my-4 border-gray-300">
 
-                                        <!-- Main modal -->
-                                        <div id="defaultModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
-                                            <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
+                                       <!-- Main modal -->
+                                        <div id="defaultModal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                                            <div class="relative w-full max-w-2xl max-h-full">
                                                 <!-- Modal content -->
                                                 <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                                                     <!-- Modal header -->
-                                                    <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="defaultModal">
-                                                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                                                        <span class="sr-only">Close modal</span>
-                                                    </button>
-                                                    <div class="flex justify-between items-start p-5 rounded-t border-b dark:border-gray-600">
-                                                        <h3 class="text-xl font-semibold text-gray-900 lg:text-2xl dark:text-white">
+                                                    <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                                                        <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
                                                             Agregar Viaje
                                                         </h3>
+                                                        <button type="button" class="hidden text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="Modal">
+                                                            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                                            </svg>
+                                                            <span class="sr-only">Close modal</span>
+                                                        </button>
                                                     </div>
                                                     <!-- Modal body -->
                                                     <div class="p-6 bg-white border-b border-gray-200">                           
                                     
-                                                        @component('components.nuevosViajesComps.form-nuevo-viaje')
+                                                        @component('components.nuevosViajesComps.form-nuevo-viaje', ['inputs_editables' => $inputs_editables])
                                                         @endcomponent
                                 
                                                     </div>                   
--- resources/views/components/nuevosViajesComps/form-nuevo-viaje.blade.php
+++ resources/views/components/nuevosViajesComps/form-nuevo-viaje.blade.php
@@ -1,3 +1,5 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
 <form action="/admin/create" method="POST">
     @csrf
     
@@ -8,24 +10,55 @@
     
     <div class="mb-6">
         <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Origen</label>
-        {{-- <input type="text" id="salida" name="salida" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"required> --}}
-        <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-gray-700 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-blue-800" type="button">
-            <span class="mr-2">Elegir Localidad</span>
-            <svg class="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-            </svg>
-        </button>
+    <div class="relative inline-block text-left">
+       
+            <button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" class="text-gray-700 bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-gray-800 dark:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-blue-800" type="button">
+                <span id="selectedOption">Seleccionar Localidad</span>
+            </button>
         
-        <!-- Dropdown menu -->
-        <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-            <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                @foreach ($inputs_editables->origen as $origen)
-                <li>
-                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{$origen}}</a>
-                </li>
-            @endforeach   
+            <!-- Dropdown menu -->
+            <div id="dropdownHover" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownHoverButton">
+                    @foreach ($inputs_editables as $input_editable)
+                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ $input_editable->origen }}</a>
+                    @endforeach
+                </ul>
+            </div>
+
+            <script>
+                // Obtén una referencia al botón y al span donde se muestra la opción seleccionada
+                const dropdownButton = document.getElementById("dropdownHoverButton");
+                const selectedOptionSpan = document.getElementById("selectedOption");
+            
+                // Agrega un evento click al botón
+                dropdownButton.addEventListener("click", () => {
+                    // Abre o cierra el menú al hacer clic en el botón
+                    document.getElementById("dropdownHover").classList.toggle("hidden");
+                });
+            
+                // Agrega un evento click a cada opción dentro del menú
+                const options = document.querySelectorAll("#dropdownHover a");
+                options.forEach(option => {
+                    option.addEventListener("click", () => {
+                        // Cuando se hace clic en una opción, actualiza el texto del span
+                        selectedOptionSpan.textContent = option.textContent;
+                        // Cierra el menú
+                        document.getElementById("dropdownHover").classList.add("hidden");
+                    });
+                });
+            </script>
+            
+
+    </div>
+
+          o 
+        <div class="mt-4">
+            <input type="text" id="salida" name="salida" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Agregar localidad nueva" required>
         </div>
-    </div> 
+    </div>
+    
+    
+
 
     <div class="mb-6">
         <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fecha Llegada</label>
@@ -34,22 +67,28 @@
     
     <div class="mb-6">
         <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Destino</label>
-        {{-- <input type="text" id="llegada" name="llegada" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"required> --}}
-        <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-gray-700 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-blue-800" type="button">
+        <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-gray-700 bg-white border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-gray-800 dark:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-blue-800" type="button">
             <span class="mr-2">Elegir Localidad</span>
             <svg class="w-2.5 h-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
             </svg>
-        </button>
+        </button>        
         
         <!-- Dropdown menu -->
         <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
             <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                @foreach ($inputs_editables->destino as $destino)
-                <li>
-                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{$destino}}</a>
-                </li>
-            @endforeach   
+                @if(isset($inputs_editables->destino))
+                    {{-- <select name="opcion_seleccionada2"> --}}
+                        @foreach ($inputs_editables->destino as $destino)
+                            <option value="{{ $destino }}">{{ $destino }}</option>
+                        @endforeach
+                    {{-- </select> --}}
+                @endif
+  
+        </div>
+        o 
+        <div class="mt-4">
+            <input type="text" id="llegada" name="llegada" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Agregar localidad nueva" required>
         </div>
     </div>
 
