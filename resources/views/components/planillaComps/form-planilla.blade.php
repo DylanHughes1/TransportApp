64aa0cc31e2aaca903c49edf89073ca358e5680c
Dylan Hughes <dylan_21174@hotmail.com>
2024-01-15 12:25:31 -0300

    Fix division por cero

--- resources/views/components/planillaComps/form-planilla.blade.php
+++ resources/views/components/planillaComps/form-planilla.blade.php
@@ -37,9 +37,7 @@
                 {{($viaje->carga_kg/1000) * $viaje->TN}}
             </td>
             <td class="px-6 py-4">
-                @if ($viaje->km_llegada - $viaje->km_salida !== 0)
-                    {{ number_format((($viaje->carga_kg/1000) * $viaje->TN) / ($viaje->km_llegada - $viaje->km_salida), 2) }}
-                @endif
+                {{ number_format((($viaje->carga_kg/1000) * $viaje->TN) / max(1, ($viaje->km_llegada - $viaje->km_salida)), 2) }}
             </td>            
             <td class="px-6 py-4">
                 <a href="#" data-modal-toggle="modalCombustible{{ $key }}" id="verMasLink" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
