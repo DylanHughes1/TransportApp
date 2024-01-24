252fef0659b279283f5bab841fc9eec95ec9e77b
Dylan Hughes <dylan_21174@hotmail.com>
2024-01-24 16:38:44 -0300

    Opcion de eliminar a todos los viajes

--- resources/views/components/viajesAsignadosComps/table-viajes-asignados.blade.php
+++ resources/views/components/viajesAsignadosComps/table-viajes-asignados.blade.php
@@ -100,17 +100,34 @@
                         <td class="px-6 py-4  text-gray-900 flex items-center space-x-3">
                                 <button type="submit" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Actualizar</button>
                             </form>
-                            @if($viaje->truckdriver_id == null)
-                                <form action="/admin/cancelar/{{$viaje->id}}" method="POST">
-                                    @csrf
-                                    @method('delete')
-                                    <button type="submit" class="font-medium text-red-600 dark:text-red-500 hover:underline">Cancelar</button>
-                                </form>
-                            @endif
-
+            
+                            <a href="#" data-modal-toggle="modalEliminar{{ $key }}" class="font-medium text-red-600 dark:text-red-500 hover:underline">Eliminar</a>
                         </td>
                     
                 </tr>
+
+                <div id="modalEliminar{{ $key }}" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
+                    <div class="relative p-4 w-full max-w-md max-h-full">
+                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                            <div class="p-4 md:p-5 text-center">
+                                <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                                </svg>
+                                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">¿Está seguro que quiere eliminar este viaje?</h3>
+                                
+                                <form action="/admin/cancelar/{{$viaje->id}}" method="POST">
+                                    @csrf
+                                    @method('delete')
+                                    <button type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2">
+                                        Si, estoy seguro
+                                    </button>
+                                </form>
+                                
+                                <button data-modal-toggle="modalEliminar{{ $key }}" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancelar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
             <!-- Main modal -->
             <div id="modalCombustible{{ $key }}" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
