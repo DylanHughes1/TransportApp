4a1f4421322214b7f8799eeafbf54809e8c4f3f7
DylanHughes1 <dylan_21174@hotmail.com>
2024-08-13 10:31:24 -0300

    Mejora visual y cierre de modals en POST

--- resources/views/components/viajesAsignadosComps/table-viajes-asignados.blade.php
+++ resources/views/components/viajesAsignadosComps/table-viajes-asignados.blade.php
@@ -152,7 +152,7 @@
                             <form action="/admin/cancelar/{{$viaje->id}}" method="POST">
                                 @csrf
                                 @method('delete')
-                                <button type="submit"
+                                <button type="submit" data-modal-toggle="modalEliminar{{ $key }}"
                                     class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2">
                                     Si, estoy seguro
                                 </button>
