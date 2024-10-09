f7811cb7692574b6b79f8408b28bbbd99327ed83
Dylan Hughes <dylan_21174@hotmail.com>
2024-10-09 14:32:07 -0300

    Separacion visual planilla choferes y de empresa

--- resources/views/components/planillaComps/seleccion-chofer.blade.php
+++ resources/views/components/planillaComps/seleccion-chofer.blade.php
@@ -1,7 +1,7 @@
 <label for="choferes" class="text-center block mb-2 text-sm font-medium text-gray-900 dark:text-white">Seleccione un
     Chofer</label>
 
-<div>
+<div class="py-3">
     <input type="text" id="search"
         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Filtrar nombre del chofer" oninput="filterDrivers()">
