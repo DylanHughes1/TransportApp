95770e5c2b47bfd35e5cdf8a9a62fbe14cdce20e
Dylan Hughes <71235743+DylanHughes1@users.noreply.github.com>
2025-09-04 11:29:07 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-
+/*
            'mysql' => [
            'driver' => 'mysql',
            'url' => env('DATABASE_URL'),
@@ -61,7 +61,7 @@ return [
          'options' => extension_loaded('pdo_mysql') ? array_filter([
                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
            ]) : [],
-        ],
+        ],*/
 
         //  'pgsql' => [
         //      'driver' => 'pgsql',
